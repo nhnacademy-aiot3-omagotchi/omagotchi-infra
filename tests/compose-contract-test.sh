50e558cdcabdfbde5937a5e9265d428d2f72cacd
woalshue <sonjaemin33@gmail.com>
2026-09-05 01:32:26 +0900

    chore: AI 채팅 읽기 타임아웃을 50초로 연장

--- tests/compose-contract-test.sh
+++ tests/compose-contract-test.sh
@@ -51,7 +51,7 @@ assert_compose_contract '
   and (.services["learning-service"].environment.COMMUNITY_ATTACHMENT_BUCKET | nonempty)
   and .services["learning-service"].environment.MINIO_BUCKET == null
   and .services["learning-service"].environment.OLLAMA_READ_TIMEOUT == "60s"
-  and (.services.frontend.environment.AI_CHAT_READ_TIMEOUT | nonempty)
+  and .services.frontend.environment.AI_CHAT_READ_TIMEOUT == "50s"
 ' "서비스 런타임 설정 연결 계약이 일치하지 않습니다."
 
 assert_compose_contract '
